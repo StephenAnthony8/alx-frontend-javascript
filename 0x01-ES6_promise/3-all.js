4f29c5b92670e29413f0c77c7cc820067d459432
StephenAnthony8 <anthonysteve02@gmail.com>
2024-04-23 23:45:31 +0300

    q3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+export default async function handleProfileSignup() {
   try {
-    Promise.all([uploadPhoto(), createUser()])
+    await Promise.all([uploadPhoto(), createUser()])
       .then((data) => {
         console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
       });
