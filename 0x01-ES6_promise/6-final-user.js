c54cbeebb388acfdb7e6fec2c345e509c74abe69
StephenAnthony8 <anthonysteve02@gmail.com>
2024-04-24 00:32:55 +0300

    q5 & 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,12 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const names = await signUpUser(firstName, lastName);
-    const file = await uploadPhoto(fileName);
-  } catch (e) {
-    return (Error());
-  }
-
-  return [names, file];
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((data) => ({ status: data.status, value: data.value }));
 }
