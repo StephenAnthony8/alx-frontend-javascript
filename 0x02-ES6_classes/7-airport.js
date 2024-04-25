bbc52992bfce502f779744ab13186a7b37b43539
StephenAnthony8 <anthonysteve02@gmail.com>
2024-04-26 01:41:54 +0300

    q7

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,6 +4,22 @@ export default class Airport {
     this._code = code;
   }
 
+  get name() {
+    return this._name;
+  }
+
+  set name(arg) {
+    this._name = arg;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(arg) {
+    this._code = arg;
+  }
+
   toString() {
     return `[Object ${this._code}]`;
   }
