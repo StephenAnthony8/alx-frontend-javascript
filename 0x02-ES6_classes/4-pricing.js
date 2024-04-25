c8f2d2500789ece9392f5ef71302a265f0e6ed0b
StephenAnthony8 <anthonysteve02@gmail.com>
2024-04-26 00:05:50 +0300

    q4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,53 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (!(typeof amount === 'number')) {
+      throw new TypeError('Not a Number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Not a Currency Object');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // amount
+  get amount() {
+    return this.amount;
+  }
+
+  set amount(arg) {
+    if (!(arg instanceof Number)) {
+      throw new TypeError('Not a Number');
+    }
+    this._amount = arg;
+  }
+
+  // currency
+  get currency() {
+    return this.currency;
+  }
+
+  set currency(arg) {
+    if (!(arg instanceof Currency)) {
+      throw new TypeError('Not a Currency Object');
+    }
+    this._currency = arg;
+  }
+
+  displayFullPrice() {
+    // returns the attributes in format amount currency
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    for (const num of [amount, conversionRate]) {
+      if (!(num instanceof Number)) {
+        throw new TypeError('Not a number');
+      }
+    }
+    return (amount * conversionRate);
+  }
+}
