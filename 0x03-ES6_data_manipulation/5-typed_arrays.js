407732b56b17ffa21f54dce73b55e78e5c0a960f
StephenAnthony8 <anthonysteve02@gmail.com>
2024-04-30 06:49:48 +0300

    Mandatory questions : Js Data structures

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
   /* returns a new ArrayBuffer of {length} with an Int8 {value} at {position} */
+  if (length <= 0) {
+    throw new Error('Position outside range');
+  }
   const rtnArr = new Int8Array(length);
 
   try {
-    if (position < length) {
+    if (position < length && position >= 0) {
       rtnArr[position] = value;
     } else {
       throw new Error();
