6de6ffa4766188b52e5ea0f17138f3b9eaf9db0c
StephenAnthony8 <anthonysteve02@gmail.com>
2024-04-30 10:03:59 +0300

    Mandatory questions : Js Data structures

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,13 @@
 export default function cleanSet(set, startString) {
   /* returns a joined string with all words that begin with startString */
   const rtnString = [];
-
-  set.forEach((x) => {
-    if (x.startsWith(startString) && startString !== '') {
-      rtnString.push(x.substring(startString.length));
-    }
-  });
+  if ((set instanceof Set) && (typeof startString === 'string')) {
+    set.forEach((x) => {
+      if (x.startsWith(startString) && startString !== '') {
+        rtnString.push(x.substring(startString.length));
+      }
+    });
+  }
 
   return rtnString.join('-');
 }
