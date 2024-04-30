11370e5e4d1be6e4567126e9fcc0715c92d65987
StephenAnthony8 <anthonysteve02@gmail.com>
2024-04-30 09:53:18 +0300

    Advanced questions : Js Data structures

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,18 +1,15 @@
-const weakMap = new weakMap();
+export const weakMap = new WeakMap();
 
-weakMap.set({protocol: 'http', name: 'getUsers'}, 0);
+weakMap.set({ protocol: 'http', name: 'getUsers' }, 0);
 
-function queryAPI(keyRef) {
+export function queryAPI(keyRef) {
   /* helps track number of times an object is called */
 
-  const count = (weakMap.has(keyRef) ? weakMap.get(keyRef) + 1 : 0);
+  const count = (weakMap.has(keyRef) ? weakMap.get(keyRef) + 1 : 1);
 
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   } else {
     weakMap.set(keyRef, count);
   }
-
 }
-
-export { weakMap, queryAPI};
