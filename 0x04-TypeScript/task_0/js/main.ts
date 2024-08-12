1b937be4ad05ec957a3e72e1b99104d7adff25fc
StephenAnthony8 <anthonysteve02@gmail.com>
2024-08-12 12:24:56 +0300

    task 0: Creation an interface for a student

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,49 @@
+/* Student interface creation */
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+}
+/* 
+const student1: Student = {
+	firstName: "Grace",
+	lastName: "Letiwa",
+	age: 27,
+	location: "Nairobi"
+};
+ */
+
+/* Student array */
+const studentsList: Student[] = [
+	{firstName: "John", lastName: "Doe", age: 27, location: "Nairobi"},
+	{firstName: "Abraham", lastName: "Loncoln", age: 27, location: "Nairobi"}
+];
+
+/* table rendering */
+
+window.onload = () => {
+	
+	/* create table */
+	const studentTable = document.createElement("table");
+	studentsList.forEach((student) => {
+
+		/* create tr element */
+		const tabRow = document.createElement("tr");
+
+		/* create td elements with student content */
+		const rowName = document.createElement("td")
+		rowName.append(student.firstName);
+		const rowLocation = document.createElement("td")
+		rowLocation.append(student.location);
+
+		/* append td elements to tr */
+		tabRow.appendChild(rowName);
+		tabRow.appendChild(rowLocation);
+		
+		/* append tr elements to table element */
+		studentTable.appendChild(tabRow);
+	});
+};
+
+
